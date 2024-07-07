bbb713522c676c0c96d03210a03dbadfc92625a2
Olzhas Kibassov <olzhas0544@gmail.com>
2024-07-08 02:16:28 +0500

    adding method testAddUserWithValidTeam()

--- src/test/java/com/orange/hw4/service/UserServiceImplTest.java
+++ src/test/java/com/orange/hw4/service/UserServiceImplTest.java
@@ -3,9 +3,16 @@ package com.orange.hw4.service;
 import com.orange.hw4.repository.UserRepository;
 import com.orange.hw4.validation.UserValidator;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDate;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
 
 public class UserServiceImplTest {
     @Mock
@@ -20,5 +27,13 @@ public class UserServiceImplTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testAddUserWithValidTeam() throws Exception {
+        doNothing().when(userValidator).validateTeam(anyString());
+        doNothing().when(userRepository).addUser(anyString(), anyString(), any(LocalDate.class), anyString());
+
+
     }
 }
