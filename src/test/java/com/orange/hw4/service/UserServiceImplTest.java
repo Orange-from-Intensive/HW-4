fb5c8e3a4ae304f96b34ef44b4b1516f12380cad
Olzhas Kibassov <olzhas0544@gmail.com>
2024-07-08 02:13:52 +0500

    adding void method setUp()

--- src/test/java/com/orange/hw4/service/UserServiceImplTest.java
+++ src/test/java/com/orange/hw4/service/UserServiceImplTest.java
@@ -1,5 +1,24 @@
 package com.orange.hw4.service;
 
-public class UserServiceImplTest {
+import com.orange.hw4.repository.UserRepository;
+import com.orange.hw4.validation.UserValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+public class UserServiceImplTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserValidator userValidator;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 }
