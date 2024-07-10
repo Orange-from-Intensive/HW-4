e54183fc842a448539885a15c014521dfb90ff93
Olzhas Kibassov <olzhas0544@gmail.com>
2024-07-11 00:19:57 +0500

    adding assert equals and adding comments

--- src/test/java/com/orange/hw4/service/JdbcJournalRepositoryTest.java
+++ src/test/java/com/orange/hw4/service/JdbcJournalRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.orange.hw4.service;
 
+import com.orange.hw4.model.Journal;
 import com.orange.hw4.model.User;
 import com.orange.hw4.repository.JdbcJournalRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,8 +10,11 @@ import org.junit.jupiter.api.Test;
 import java.sql.*;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 public class JdbcJournalRepositoryTest {
     private Connection connection;
@@ -66,6 +70,33 @@ public class JdbcJournalRepositoryTest {
         when(resultSet.getDouble("user_two_mark")).thenReturn(3.0);
         when(resultSet.getLong("id")).thenReturn(1L);
 
-        
+        List<Journal> journals = repository.getJournalFeed();
+
+//        assertEquals(1, journals.size());
+//        Journal journal = journals.get(0);
+//        assertEquals(LocalDate.of(2023, 1, 1), journal.getLessonDate());
+//        assertEquals(1L, journal.getUserOneId());
+//        assertEquals(2L, journal.getUserTwoId());
+//        assertEquals(2.0, journal.getUserOneMark());
+//        assertEquals(3.0, journal.getUserTwoMark());
+    }
+
+    @Test
+    public void testGetAllUserOpponents() throws SQLException {
+        PreparedStatement statement = mock(PreparedStatement.class);
+        ResultSet resultSet = mock(ResultSet.class);
+        when(connection.prepareStatement(any(String.class))).thenReturn(statement);
+        when(statement.executeQuery()).thenReturn(resultSet);
+
+        when(resultSet.next()).thenReturn(true).thenReturn(false);
+        when(resultSet.getLong("user_one_id")).thenReturn(1L);
+        when(resultSet.getLong("user_two_id")).thenReturn(2L);
+        when(resultSet.getInt("count")).thenReturn(3);
+
+        User user = new User(1L, "John", "Doe", LocalDate.of(1990, 1, 1), "Team A");
+        Map<Long, Map<Long, Integer>> userOpponents = repository.getAllUserOpponents(user);
+
+        assertEquals(1, userOpponents.size());
+        assertEquals(3, userOpponents.get(1L).get(2L).intValue());
     }
 }
