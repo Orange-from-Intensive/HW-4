a01dd791072954475a4cf75dbfd2af4faa609958
Olzhas Kibassov <olzhas0544@gmail.com>
2024-07-11 00:34:28 +0500

    add testAddUser method with mock objects

--- src/test/java/com/orange/hw4/service/JdbcUserRepositoryTest.java
+++ src/test/java/com/orange/hw4/service/JdbcUserRepositoryTest.java
@@ -4,11 +4,14 @@ import com.orange.hw4.repository.JdbcUserRepository;
 import lombok.extern.slf4j.Slf4j;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 
 import java.sql.Connection;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import static org.mockito.Mockito.*;
 
@@ -21,5 +24,20 @@ public class JdbcUserRepositoryTest {
     public void setUp() throws SQLException {
         connection = mock(Connection.class);
         repository = new JdbcUserRepository(connection);
+    }
+
+    @Test
+    public void testAddUser() throws SQLException {
+        PreparedStatement statement = mock(PreparedStatement.class);
+        when(connection.prepareStatement(any(String.class))).thenReturn(statement);
+
+        String name = "John";
+        String surName = "Doe";
+        LocalDate birthDate = LocalDate.of(1990, 1, 1);
+        String team = "Team A";
+
+        repository.addUser(name, surName, birthDate, team);
+
+       
     }
 }
